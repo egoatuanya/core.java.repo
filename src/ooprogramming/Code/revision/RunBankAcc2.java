b9f4a37f08886b3e29a9d6c052fec373474771f6
Ego.Atuanya <egoatuanya@yahoo.com>
2018-10-15 17:28:12 +0100

    Changes made to BankAcc2

--- src/ooprogramming/Code/revision/RunBankAcc2.java
+++ src/ooprogramming/Code/revision/RunBankAcc2.java
@@ -4,8 +4,12 @@ public class RunBankAcc2
 {
     public static void main(String [] args)
     {
-        BankAcc2 myaccount = new BankAcc2(4500);
-        int mybalance = myaccount.accountBalance();
-        System.out.println(mybalance);
+       BankAcc2 myaccount = new BankAcc2(4500);
+       double initialbalance = myaccount.accountBalance();
+       System.out.println(initialbalance);
+       double mybalance = myaccount.accountWithdrawal(250.50);
+       System.out.println(mybalance);
+       double depositbalance = myaccount.accountDeposit(500);
+       System.out.println(depositbalance);
     }
 }
