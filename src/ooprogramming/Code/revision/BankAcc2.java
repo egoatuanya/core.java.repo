8da17891b64cd553ffc15f735a32c5b5d9a938a5
Ego.Atuanya <egoatuanya@yahoo.com>
2018-10-15 17:25:40 +0100

    Changes made to BankAcc2

--- src/ooprogramming/Code/revision/BankAcc2.java
+++ src/ooprogramming/Code/revision/BankAcc2.java
@@ -4,23 +4,67 @@ import java.util.Scanner;
 public class BankAcc2
 {
     private String pinnum;
-    private int balance;
-    private int withdraw;
-    private int deposit;
+    private double balance;
+    private double withdraw;
+    private double deposit;
 
-    public BankAcc2(int balance)
+    public BankAcc2(double balance)
     {
         this.pinnum = "7890";
         this.balance = balance;
         this.withdraw = withdraw;
         this.deposit= deposit;
     }
+    public double accountDeposit(double deposit)
+    {
+        String mypin = getPin();
+        if (mypin.equals(pinnum))
+        {
+            this.balance = balance + deposit;
+            return this.balance;
+        }
+        else
+        {
+            return 0.0;
+        }
+    }
 
-    public int accountBalance()
+    public double accountWithdrawal(double withdraw)
     {
         String mypin = getPin();
 
-        return balance;
+            if (mypin.equals(pinnum)) {
+                if (withdraw < balance) {
+                    this.balance = balance - withdraw;
+                    return this.balance;
+                } else {
+                    System.out.println("You have insufficient funds to make a withdrawal");
+                    return 0.0;
+                }
+            }
+            else
+            {
+                System.out.println("You have entered an incorrect pin number");
+                return 0.0;
+
+            }
+        }
+
+
+    public double accountBalance()
+    {
+        String mypin = getPin();
+        if (mypin.equals(pinnum))
+        {
+            return balance;
+        }
+        else
+        {
+            return 0.0;
+        }
+
+
+
     }
     private String getPin()
     {
@@ -35,6 +79,7 @@ public class BankAcc2
         else
         {
             System.out.println("Access denied, please enter your pin number againg");
+            return "";
         }
 
 
